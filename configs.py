88b3db96856d5c1922d5fb96272e415f8df06618
KcCinemagalu <124599828+KcCinemagalu@users.noreply.github.com>
2024-01-09 21:34:25 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "your api id"))
-    API_HASH = getenv("API_HASH", "your api hash")
-    BOT_TOKEN = getenv("BOT_TOKEN", "your bot token")
-    FSUB = getenv("FSUB", "your channel username")
+    API_ID = int(getenv("API_ID", "14356452"))
+    API_HASH = getenv("API_HASH", "cac21249a0c6373a1b742afb8dbc9cb7")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6704921667:AAGKp6AJSmEcGUKiX4Jhg_2xlPO3YBHqGMc")
+    FSUB = getenv("FSUB", "KC_Moviez")
     CHID = int(getenv("CHID", "your channel id"))
     SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb url")
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Nandesha:alabal18@cluster0.6qjsxnq.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
